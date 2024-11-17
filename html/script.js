665f244bf2df9d85b0cc46369f89fef5bcefbda4
sudha <sudhaaaaabasnet@gmail.com>
2024-11-17 20:17:16 +0545

    learned new things

--- html/script.js
+++ html/script.js
@@ -1,8 +1,41 @@
-let a = 5
-let b = 10
-if (a<=b) {
-    console.log("b is greater");}
-    else {
-        console.log("a i greater");
+{
+    let a = 15
+    let b = 10
+    if (a<=b) {
+
+        console.log("b is greater");}
+
+        else {
+            console.log("a i greater");
+            }
+}
+
+
+{
+    let a = 20;
+    let b = 30;
+    if (a%2==0 && b%2==0) {
+        console.log("even");
     }
 
+        else {
+            console.log ("odd");
+        }
+}
+
+
+{
+    let age = 20
+    let result= age>=18? "adult" : "not adult";
+    console.log("result = ",result);
+
+}
+
+
+{
+    let a =prompt("enter a number");
+    let result = a%5==0? "it is divisible by 5" : "its not divisible by 5";
+    console.log ("result =",result);
+
+    
+}
